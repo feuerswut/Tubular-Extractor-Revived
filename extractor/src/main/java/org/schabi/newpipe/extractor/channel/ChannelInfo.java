Stypox/NewPipeExtractor

Replace avatar and thumbnail URLs attributes and methods to List<Image> in Infos

extractor/src/main/java/org/schabi/newpipe/extractor/channel/ChannelInfo.java
@@ -1,5 +1,26 @@
+/*
+ * Created by Christian Schabesberger on 31.07.16.
+ *
+ * Copyright (C) Christian Schabesberger 2016 <chris.schabesberger@mailbox.org>
+ * ChannelInfo.java is part of NewPipe Extractor.
+ *
+ * NewPipe Extractor is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * NewPipe Extractor is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with NewPipe Extractor.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package org.schabi.newpipe.extractor.channel;
 
+import org.schabi.newpipe.extractor.Image;
 import org.schabi.newpipe.extractor.ListExtractor.InfoItemsPage;
 import org.schabi.newpipe.extractor.ListInfo;
 import org.schabi.newpipe.extractor.NewPipe;
@@ -10,27 +31,10 @@ import org.schabi.newpipe.extractor.linkhandler.ListLinkHandler;
 import org.schabi.newpipe.extractor.stream.StreamInfoItem;
 import org.schabi.newpipe.extractor.utils.ExtractorHelper;
 
+import javax.annotation.Nonnull;
 import java.io.IOException;
-
-/*
- * Created by Christian Schabesberger on 31.07.16.
- *
- * Copyright (C) Christian Schabesberger 2016 <chris.schabesberger@mailbox.org>
- * ChannelInfo.java is part of NewPipe.
- *
- * NewPipe is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * NewPipe is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with NewPipe.  If not, see <http://www.gnu.org/licenses/>.
- */
+import java.util.Collections;
+import java.util.List;
 
 public class ChannelInfo extends ListInfo<StreamInfoItem> {
 
@@ -75,12 +79,12 @@ public class ChannelInfo extends ListInfo<StreamInfoItem> {
                 new ChannelInfo(serviceId, id, url, originalUrl, name, extractor.getLinkHandler());
 
         try {
-            info.setAvatarUrl(extractor.getAvatarUrl());
+            info.setAvatars(extractor.getAvatars());
         } catch (final Exception e) {
             info.addError(e);
         }
         try {
-            info.setBannerUrl(extractor.getBannerUrl());
+            info.setBanners(extractor.getBanners());
         } catch (final Exception e) {
             info.addError(e);
         }
@@ -119,7 +123,7 @@ public class ChannelInfo extends ListInfo<StreamInfoItem> {
         }
 
         try {
-            info.setParentChannelAvatarUrl(extractor.getParentChannelAvatarUrl());
+            info.setParentChannelAvatars(extractor.getParentChannelAvatars());
         } catch (final Exception e) {
             info.addError(e);
         }
@@ -133,15 +137,18 @@ public class ChannelInfo extends ListInfo<StreamInfoItem> {
         return info;
     }
 
-    private String avatarUrl;
     private String parentChannelName;
     private String parentChannelUrl;
-    private String parentChannelAvatarUrl;
-    private String bannerUrl;
     private String feedUrl;
     private long subscriberCount = -1;
     private String description;
     private String[] donationLinks;
+    @Nonnull
+    private List<Image> avatars = Collections.emptyList();
+    @Nonnull
+    private List<Image> banners = Collections.emptyList();
+    @Nonnull
+    private List<Image> parentChannelAvatars = Collections.emptyList();
     private boolean verified;
 
     public String getParentChannelName() {
@@ -160,28 +167,31 @@ public class ChannelInfo extends ListInfo<StreamInfoItem> {
         this.parentChannelUrl = parentChannelUrl;
     }
 
-    public String getParentChannelAvatarUrl() {
-        return parentChannelAvatarUrl;
+    @Nonnull
+    public List<Image> getParentChannelAvatars() {
+        return parentChannelAvatars;
     }
 
-    public void setParentChannelAvatarUrl(final String parentChannelAvatarUrl) {
-        this.parentChannelAvatarUrl = parentChannelAvatarUrl;
+    public void setParentChannelAvatars(@Nonnull final List<Image> parentChannelAvatars) {
+        this.parentChannelAvatars = parentChannelAvatars;
     }
 
-    public String getAvatarUrl() {
-        return avatarUrl;
+    @Nonnull
+    public List<Image> getAvatars() {
+        return avatars;
     }
 
-    public void setAvatarUrl(final String avatarUrl) {
-        this.avatarUrl = avatarUrl;
+    public void setAvatars(@Nonnull final List<Image> avatars) {
+        this.avatars = avatars;
     }
 
-    public String getBannerUrl() {
-        return bannerUrl;
+    @Nonnull
+    public List<Image> getBanners() {
+        return banners;
     }
 
-    public void setBannerUrl(final String bannerUrl) {
-        this.bannerUrl = bannerUrl;
+    public void setBanners(@Nonnull final List<Image> banners) {
+        this.banners = banners;
     }
 
     public String getFeedUrl() {

extractor/src/main/java/org/schabi/newpipe/extractor/playlist/PlaylistInfo.java
@@ -1,5 +1,6 @@
 package org.schabi.newpipe.extractor.playlist;
 
+import org.schabi.newpipe.extractor.Image;
 import org.schabi.newpipe.extractor.ListExtractor.InfoItemsPage;
 import org.schabi.newpipe.extractor.ListInfo;
 import org.schabi.newpipe.extractor.NewPipe;
@@ -11,8 +12,10 @@ import org.schabi.newpipe.extractor.linkhandler.ListLinkHandler;
 import org.schabi.newpipe.extractor.stream.StreamInfoItem;
 import org.schabi.newpipe.extractor.utils.ExtractorHelper;
 
+import javax.annotation.Nonnull;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public final class PlaylistInfo extends ListInfo<StreamInfoItem> {
@@ -103,26 +106,23 @@ public final class PlaylistInfo extends ListInfo<StreamInfoItem> {
             info.addError(e);
         }
         try {
-            info.setThumbnailUrl(extractor.getThumbnailUrl());
+            info.setThumbnails(extractor.getThumbnails());
         } catch (final Exception e) {
             info.addError(e);
         }
         try {
             info.setUploaderUrl(extractor.getUploaderUrl());
         } catch (final Exception e) {
-            info.setUploaderUrl("");
             uploaderParsingErrors.add(e);
         }
         try {
             info.setUploaderName(extractor.getUploaderName());
         } catch (final Exception e) {
-            info.setUploaderName("");
             uploaderParsingErrors.add(e);
         }
         try {
-            info.setUploaderAvatarUrl(extractor.getUploaderAvatarUrl());
+            info.setUploaderAvatars(extractor.getUploaderAvatars());
         } catch (final Exception e) {
-            info.setUploaderAvatarUrl("");
             uploaderParsingErrors.add(e);
         }
         try {
@@ -136,12 +136,12 @@ public final class PlaylistInfo extends ListInfo<StreamInfoItem> {
             uploaderParsingErrors.add(e);
         }
         try {
-            info.setSubChannelAvatarUrl(extractor.getSubChannelAvatarUrl());
+            info.setSubChannelAvatars(extractor.getSubChannelAvatars());
         } catch (final Exception e) {
             uploaderParsingErrors.add(e);
         }
         try {
-            info.setBannerUrl(extractor.getBannerUrl());
+            info.setBanners(extractor.getBanners());
         } catch (final Exception e) {
             info.addError(e);
         }
@@ -165,31 +165,37 @@ public final class PlaylistInfo extends ListInfo<StreamInfoItem> {
         return info;
     }
 
-    private String thumbnailUrl;
-    private String bannerUrl;
-    private String uploaderUrl;
-    private String uploaderName;
-    private String uploaderAvatarUrl;
+    private String uploaderUrl = "";
+    private String uploaderName = "";
     private String subChannelUrl;
     private String subChannelName;
-    private String subChannelAvatarUrl;
-    private long streamCount = 0;
+    @Nonnull
+    private List<Image> banners = Collections.emptyList();
+    @Nonnull
+    private List<Image> subChannelAvatars = Collections.emptyList();
+    @Nonnull
+    private List<Image> thumbnails = Collections.emptyList();
+    @Nonnull
+    private List<Image> uploaderAvatars = Collections.emptyList();
+    private long streamCount;
     private PlaylistType playlistType;
 
-    public String getThumbnailUrl() {
-        return thumbnailUrl;
+    @Nonnull
+    public List<Image> getThumbnails() {
+        return thumbnails;
     }
 
-    public void setThumbnailUrl(final String thumbnailUrl) {
-        this.thumbnailUrl = thumbnailUrl;
+    public void setThumbnails(@Nonnull final List<Image> thumbnails) {
+        this.thumbnails = thumbnails;
     }
 
-    public String getBannerUrl() {
-        return bannerUrl;
+    @Nonnull
+    public List<Image> getBanners() {
+        return banners;
     }
 
-    public void setBannerUrl(final String bannerUrl) {
-        this.bannerUrl = bannerUrl;
+    public void setBanners(@Nonnull final List<Image> banners) {
+        this.banners = banners;
     }
 
     public String getUploaderUrl() {
@@ -208,12 +214,13 @@ public final class PlaylistInfo extends ListInfo<StreamInfoItem> {
         this.uploaderName = uploaderName;
     }
 
-    public String getUploaderAvatarUrl() {
-        return uploaderAvatarUrl;
+    @Nonnull
+    public List<Image> getUploaderAvatars() {
+        return uploaderAvatars;
     }
 
-    public void setUploaderAvatarUrl(final String uploaderAvatarUrl) {
-        this.uploaderAvatarUrl = uploaderAvatarUrl;
+    public void setUploaderAvatars(@Nonnull final List<Image> uploaderAvatars) {
+        this.uploaderAvatars = uploaderAvatars;
     }
 
     public String getSubChannelUrl() {
@@ -232,12 +239,13 @@ public final class PlaylistInfo extends ListInfo<StreamInfoItem> {
         this.subChannelName = subChannelName;
     }
 
-    public String getSubChannelAvatarUrl() {
-        return subChannelAvatarUrl;
+    @Nonnull
+    public List<Image> getSubChannelAvatars() {
+        return subChannelAvatars;
     }
 
-    public void setSubChannelAvatarUrl(final String subChannelAvatarUrl) {
-        this.subChannelAvatarUrl = subChannelAvatarUrl;
+    public void setSubChannelAvatars(@Nonnull final List<Image> subChannelAvatars) {
+        this.subChannelAvatars = subChannelAvatars;
     }
 
     public long getStreamCount() {

feuerswut/Tubular-Extractor-Revived

extractor/src/main/java/org/schabi/newpipe/extractor/channel/ChannelInfo.java
@@ -1,6 +1,27 @@
+/*
+ * Created by Christian Schabesberger on 31.07.16.
+ *
+ * Copyright (C) Christian Schabesberger 2016 <chris.schabesberger@mailbox.org>
+ * ChannelInfo.java is part of NewPipe Extractor.
+ *
+ * NewPipe Extractor is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * NewPipe Extractor is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with NewPipe Extractor.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package org.schabi.newpipe.extractor.channel;
 
 import org.schabi.newpipe.extractor.Info;
+import org.schabi.newpipe.extractor.Image;
 import org.schabi.newpipe.extractor.NewPipe;
 import org.schabi.newpipe.extractor.StreamingService;
 import org.schabi.newpipe.extractor.exceptions.ExtractionException;
@@ -10,26 +31,6 @@ import java.io.IOException;
 import java.util.List;
 
 import javax.annotation.Nonnull;
-
-/*
- * Created by Christian Schabesberger on 31.07.16.
- *
- * Copyright (C) Christian Schabesberger 2016 <chris.schabesberger@mailbox.org>
- * ChannelInfo.java is part of NewPipe.
- *
- * NewPipe is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * NewPipe is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with NewPipe.  If not, see <http://www.gnu.org/licenses/>.
- */
 
 public class ChannelInfo extends Info {
 
@@ -64,13 +65,13 @@ public class ChannelInfo extends Info {
         final ChannelInfo info = new ChannelInfo(serviceId, id, url, originalUrl, name);
 
         try {
-            info.setAvatarUrl(extractor.getAvatarUrl());
+            info.setAvatars(extractor.getAvatars());
         } catch (final Exception e) {
             info.addError(e);
         }
 
         try {
-            info.setBannerUrl(extractor.getBannerUrl());
+            info.setBanners(extractor.getBanners());
         } catch (final Exception e) {
             info.addError(e);
         }
@@ -106,7 +107,7 @@ public class ChannelInfo extends Info {
         }
 
         try {
-            info.setParentChannelAvatarUrl(extractor.getParentChannelAvatarUrl());
+            info.setParentChannelAvatars(extractor.getParentChannelAvatars());
         } catch (final Exception e) {
             info.addError(e);
         }
@@ -132,15 +133,18 @@ public class ChannelInfo extends Info {
         return info;
     }
 
-    private String avatarUrl;
     private String parentChannelName;
     private String parentChannelUrl;
-    private String parentChannelAvatarUrl;
-    private String bannerUrl;
     private String feedUrl;
     private long subscriberCount = -1;
     private String description;
     private String[] donationLinks;
+    @Nonnull
+    private List<Image> avatars = List.of();
+    @Nonnull
+    private List<Image> banners = List.of();
+    @Nonnull
+    private List<Image> parentChannelAvatars = List.of();
     private boolean verified;
     private List<ListLinkHandler> tabs = List.of();
     private List<String> tags = List.of();
@@ -161,28 +165,31 @@ public class ChannelInfo extends Info {
         this.parentChannelUrl = parentChannelUrl;
     }
 
-    public String getParentChannelAvatarUrl() {
-        return parentChannelAvatarUrl;
+    @Nonnull
+    public List<Image> getParentChannelAvatars() {
+        return parentChannelAvatars;
     }
 
-    public void setParentChannelAvatarUrl(final String parentChannelAvatarUrl) {
-        this.parentChannelAvatarUrl = parentChannelAvatarUrl;
+    public void setParentChannelAvatars(@Nonnull final List<Image> parentChannelAvatars) {
+        this.parentChannelAvatars = parentChannelAvatars;
     }
 
-    public String getAvatarUrl() {
-        return avatarUrl;
+    @Nonnull
+    public List<Image> getAvatars() {
+        return avatars;
     }
 
-    public void setAvatarUrl(final String avatarUrl) {
-        this.avatarUrl = avatarUrl;
+    public void setAvatars(@Nonnull final List<Image> avatars) {
+        this.avatars = avatars;
     }
 
-    public String getBannerUrl() {
-        return bannerUrl;
+    @Nonnull
+    public List<Image> getBanners() {
+        return banners;
     }
 
-    public void setBannerUrl(final String bannerUrl) {
-        this.bannerUrl = bannerUrl;
+    public void setBanners(@Nonnull final List<Image> banners) {
+        this.banners = banners;
     }
 
     public String getFeedUrl() {

extractor/src/main/java/org/schabi/newpipe/extractor/playlist/PlaylistInfo.java
@@ -1,5 +1,6 @@
 package org.schabi.newpipe.extractor.playlist;
 
+import org.schabi.newpipe.extractor.Image;
 import org.schabi.newpipe.extractor.ListExtractor.InfoItemsPage;
 import org.schabi.newpipe.extractor.ListInfo;
 import org.schabi.newpipe.extractor.NewPipe;
@@ -12,6 +13,7 @@ import org.schabi.newpipe.extractor.stream.Description;
 import org.schabi.newpipe.extractor.stream.StreamInfoItem;
 import org.schabi.newpipe.extractor.utils.ExtractorHelper;
 
+import javax.annotation.Nonnull;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -109,26 +111,23 @@ public final class PlaylistInfo extends ListInfo<StreamInfoItem> {
             info.addError(e);
         }
         try {
-            info.setThumbnailUrl(extractor.getThumbnailUrl());
+            info.setThumbnails(extractor.getThumbnails());
         } catch (final Exception e) {
             info.addError(e);
         }
         try {
             info.setUploaderUrl(extractor.getUploaderUrl());
         } catch (final Exception e) {
-            info.setUploaderUrl("");
             uploaderParsingErrors.add(e);
         }
         try {
             info.setUploaderName(extractor.getUploaderName());
         } catch (final Exception e) {
-            info.setUploaderName("");
             uploaderParsingErrors.add(e);
         }
         try {
-            info.setUploaderAvatarUrl(extractor.getUploaderAvatarUrl());
+            info.setUploaderAvatars(extractor.getUploaderAvatars());
         } catch (final Exception e) {
-            info.setUploaderAvatarUrl("");
             uploaderParsingErrors.add(e);
         }
         try {
@@ -142,12 +141,12 @@ public final class PlaylistInfo extends ListInfo<StreamInfoItem> {
             uploaderParsingErrors.add(e);
         }
         try {
-            info.setSubChannelAvatarUrl(extractor.getSubChannelAvatarUrl());
+            info.setSubChannelAvatars(extractor.getSubChannelAvatars());
         } catch (final Exception e) {
             uploaderParsingErrors.add(e);
         }
         try {
-            info.setBannerUrl(extractor.getBannerUrl());
+            info.setBanners(extractor.getBanners());
         } catch (final Exception e) {
             info.addError(e);
         }
@@ -171,32 +170,38 @@ public final class PlaylistInfo extends ListInfo<StreamInfoItem> {
         return info;
     }
 
-    private String thumbnailUrl;
-    private String bannerUrl;
-    private String uploaderUrl;
-    private String uploaderName;
-    private String uploaderAvatarUrl;
+    private String uploaderUrl = "";
+    private String uploaderName = "";
     private String subChannelUrl;
     private String subChannelName;
-    private String subChannelAvatarUrl;
-    private long streamCount = 0;
     private Description description;
+    @Nonnull
+    private List<Image> banners = List.of();
+    @Nonnull
+    private List<Image> subChannelAvatars = List.of();
+    @Nonnull
+    private List<Image> thumbnails = List.of();
+    @Nonnull
+    private List<Image> uploaderAvatars = List.of();
+    private long streamCount;
     private PlaylistType playlistType;
 
-    public String getThumbnailUrl() {
-        return thumbnailUrl;
+    @Nonnull
+    public List<Image> getThumbnails() {
+        return thumbnails;
     }
 
-    public void setThumbnailUrl(final String thumbnailUrl) {
-        this.thumbnailUrl = thumbnailUrl;
+    public void setThumbnails(@Nonnull final List<Image> thumbnails) {
+        this.thumbnails = thumbnails;
     }
 
-    public String getBannerUrl() {
-        return bannerUrl;
+    @Nonnull
+    public List<Image> getBanners() {
+        return banners;
     }
 
-    public void setBannerUrl(final String bannerUrl) {
-        this.bannerUrl = bannerUrl;
+    public void setBanners(@Nonnull final List<Image> banners) {
+        this.banners = banners;
     }
 
     public String getUploaderUrl() {
@@ -215,12 +220,13 @@ public final class PlaylistInfo extends ListInfo<StreamInfoItem> {
         this.uploaderName = uploaderName;
     }
 
-    public String getUploaderAvatarUrl() {
-        return uploaderAvatarUrl;
+    @Nonnull
+    public List<Image> getUploaderAvatars() {
+        return uploaderAvatars;
     }
 
-    public void setUploaderAvatarUrl(final String uploaderAvatarUrl) {
-        this.uploaderAvatarUrl = uploaderAvatarUrl;
+    public void setUploaderAvatars(@Nonnull final List<Image> uploaderAvatars) {
+        this.uploaderAvatars = uploaderAvatars;
     }
 
     public String getSubChannelUrl() {
@@ -239,12 +245,13 @@ public final class PlaylistInfo extends ListInfo<StreamInfoItem> {
         this.subChannelName = subChannelName;
     }
 
-    public String getSubChannelAvatarUrl() {
-        return subChannelAvatarUrl;
+    @Nonnull
+    public List<Image> getSubChannelAvatars() {
+        return subChannelAvatars;
     }
 
-    public void setSubChannelAvatarUrl(final String subChannelAvatarUrl) {
-        this.subChannelAvatarUrl = subChannelAvatarUrl;
+    public void setSubChannelAvatars(@Nonnull final List<Image> subChannelAvatars) {
+        this.subChannelAvatars = subChannelAvatars;
     }
 
     public long getStreamCount() {